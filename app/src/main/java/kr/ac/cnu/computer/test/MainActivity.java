c19fc9ac5c39ccd5ffb78842d0ba1416e05eb6cf
SeungYun0809 <keunsy@naver.com>
2022-05-15 17:40:00 +0900

    수의사 네비게이션바

---
+++ app/src/main/java/kr/ac/cnu/computer/test/MainActivity.java
@@ -0,0 +1,45 @@
+package kr.ac.cnu.computer.test;
+
+import android.view.MenuItem;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private vet_FragmentDiagnosis vet_fragmentDiagnosis = new vet_FragmentDiagnosis();
+    private FragmentMyPage fragmentMyPage = new FragmentMyPage();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, vet_fragmentDiagnosis).commitAllowingStateLoss();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnItemSelectedListener(new ItemSelectedListener());
+
+    }
+    class ItemSelectedListener implements BottomNavigationView.OnItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+
+                case R.id.vet_Diagnosis:
+                    transaction.replace(R.id.frameLayout, vet_fragmentDiagnosis).commitAllowingStateLoss();
+                    break;
+                case R.id.MyPage:
+                    transaction.replace(R.id.frameLayout, fragmentMyPage).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/kr/ac/cnu/computer/test/vet_FragmentDiagnosis.java
@@ -0,0 +1,15 @@
+package kr.ac.cnu.computer.test;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class vet_FragmentDiagnosis extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+}
